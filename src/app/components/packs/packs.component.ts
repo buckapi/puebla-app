4858c4c74070c28ae574a14cf5e14bc98df773a0
buckapi <buckapis@gmail.com>
2023-08-02 17:23:55 -0500

    slider platos

--- src/app/components/packs/packs.component.ts
+++ src/app/components/packs/packs.component.ts
@@ -6,6 +6,8 @@ import { Yeoman } from '@app/services/yeoman.service';
 import { AuthRESTService } from '@app/services/authREST.service';
 import { HttpClient } from '@angular/common/http';
 
+import { AbstractControl, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+
 import { DeviceDetectorService } from 'ngx-device-detector';
 declare var intlTelInput: any; 
 @Component({
@@ -14,9 +16,15 @@ declare var intlTelInput: any;
   styleUrls: ['./packs.component.css']
 })
 export class PacksComponent implements OnInit, AfterViewInit {
-  
+  form: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+  });
+
   @ViewChild('phoneInput', { static: true }) phoneInput!: ElementRef; // Obtén una referencia al elemento del teléfono en el formulario
   deviceInfo:any;
+  menssage:string='';
+  errorMenssage:boolean=false;
   name: string = '';
   phone:string='';
   email: string = '';
@@ -28,17 +36,26 @@ export class PacksComponent implements OnInit, AfterViewInit {
   isHovered: boolean = false;
   showDescription: boolean = false;
   showScrollUpButton = false;
-
+  registrationForm: FormGroup;
+  submitted: boolean = false;
   constructor(
+
+    private formBuilder: FormBuilder,
     private deviceService: DeviceDetectorService,
     private authRESTService: AuthRESTService,
     private router:Router,
     public yeoman:Yeoman
   ) {
+    this.registrationForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      phone: new FormControl('', [Validators.required])
+    });
     this.epicFunction();
     this.checkIfMobile();
     window.addEventListener('resize', () => {
-      this.checkIfMobile();
+    this.checkIfMobile();
     });
     this.packs=PACKS
     this.categories=CATEGORIES
@@ -46,11 +63,11 @@ export class PacksComponent implements OnInit, AfterViewInit {
    checkIfMobile() {
     this.isMobile = window.innerWidth <= 767;
   }
+  
   request() {
     const phoneNumber = '+5212216482271';
     const message = `Hello, I want to book the package "${this.yeoman.preview.name}"`;
     const encodedMessage = encodeURIComponent(message);
-
     const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
     window.open(url, '_blank');
   }
@@ -58,55 +75,70 @@ export class PacksComponent implements OnInit, AfterViewInit {
     const phoneNumber = '+5212216482271';
     const message = `Hello, my name is ${this.name} (${this.email} )and I want to book the package "${this.yeoman.preview.name}"`;
     const encodedMessage = encodeURIComponent(message);
-
     const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
     window.open(url, '_blank');
   }
-  registerUser() {
-    this.sending = true;
-    // Llama al método registerUser del servicio para registrar al usuario
-    this.authRESTService.registerUser(this.email, this.password, this.name).subscribe(
-      (userData) => {
-        // Aquí puedes manejar la respuesta de la API después de registrar al usuario exitosamente
-        const message = `Usuario registrado exitosamente.\nNombre: ${this.name}\nEmail: ${this.yeoman.phone}`;
-        // alert(message);
-        this.sending = false;
-        this.authRESTService.setUser(userData);
-        // this.authRESTService.setToken(userData.id);
-        // this.yeoman.currentUser.email=this.email;
-        
-        // this.authRESTService.setToken(token);
-        // Llama al método createBooking del servicio para crear el nuevo documento en /booking
-        if(this.yeoman.type=='pack'){this.yeoman.preview.type="pack";}
-        if(this.yeoman.type=='transportation'){this.yeoman.preview.type="transportation";}
-        this.authRESTService.createBooking(this.name, this.email, this.yeoman.preview.name,this.yeoman.preview.type, this.yeoman.phone/* phone */).subscribe(
-          (bookingData) => {
-            // Aquí puedes manejar la respuesta de la API después de crear el nuevo documento en /booking
-            console.log('Nuevo documento creado en /booking:', bookingData);
-            this.yeoman.userType='customer';
-            this.yeoman.email=bookingData.email;
-            this.authRESTService.getAllBookings().subscribe((data=>{
-              this.yeoman.bookings=data;
-              console.log("datica:  "+this.yeoman.bookings);
-              this.router.navigate(['my'])
-            }));
-            
-          },
-          (error) => {
-            // Manejo de errores si la API devuelve algún error durante la creación del documento en /booking
-            console.error('Error al crear nuevo documento en /booking:', error);
-          }
-        );
 
-        this.sendMessageToWhatsApp();
-        // Si deseas redirigir al usuario a otra página después del registro, puedes hacerlo aquí.
-      },
-      (error) => {
-        this.sending = false;
-        // Manejo de errores si la API devuelve algún error durante el registro
-        console.error('Error al registrar al usuario:', error);
+  get f(): { [key: string]: AbstractControl } {
+    return this.registrationForm.controls;
+  }
+  registerUserSug() {
+    this.submitted = true;
+    if (this.registrationForm.invalid) {
+      return;
+    }
+    this.errorMenssage=false;
+
+     
+      // Realizar acciones cuando el formulario es válido, por ejemplo, enviar los datos al servidor
+      this.sending = true;
+      // Llama al método registerUser del servicio para registrar al usuario
+      this.authRESTService.registerUser(this.registrationForm.value.email, this.registrationForm.value.password, this.registrationForm.value.name).subscribe(
+        (userData) => {
+          // Aquí puedes manejar la respuesta de la API después de registrar al usuario exitosamente
+          const message = `Usuario registrado exitosamente.\nNombre: ${this.name}\nEmail: ${this.yeoman.phone}`;
+          // alert(message);
+          this.sending = false;
+          this.authRESTService.setUser(userData);
+          // this.authRESTService.setToken(userData.id);
+          // this.yeoman.currentUser.email=this.email;
+          
+          // this.authRESTService.setToken(token);
+          // Llama al método createBooking del servicio para crear el nuevo documento en /booking
+          if(this.yeoman.type=='pack'){this.yeoman.preview.type="pack";}
+          if(this.yeoman.type=='transportation'){this.yeoman.preview.type="transportation";}
+          this.authRESTService.createBooking(this.name, this.email, this.yeoman.preview.name,this.yeoman.preview.type, this.yeoman.phone/* phone */).subscribe(
+            (bookingData) => {
+              // Aquí puedes manejar la respuesta de la API después de crear el nuevo documento en /booking
+              console.log('Nuevo documento creado en /booking:', bookingData);
+              this.yeoman.userType='customer';
+              this.yeoman.email=bookingData.email;
+              this.authRESTService.getAllBookings().subscribe((data=>{
+                this.yeoman.bookings=data;
+                console.log("datica:  "+this.yeoman.bookings);
+                this.router.navigate(['my'])
+              }));
+              
+            },
+            (error) => {
+              // Manejo de errores si la API devuelve algún error durante la creación del documento en /booking
+              console.error('Error al crear nuevo documento en /booking:', error);
+            }
+          );
+  
+          this.sendMessageToWhatsApp();
+          // Si deseas redirigir al usuario a otra página después del registro, puedes hacerlo aquí.
+        },
+        (error) => {
+          this.sending = false;
+          // Manejo de errores si la API devuelve algún error durante el registro
+          console.error('Error al registrar al usuario:', error);
+        }
+      );
       }
-    );
+
+  registerUser() {
+  
   }
   onDeleteAllBranches(): void {
     this.authRESTService.deleteAllBranches().subscribe(
--- src/app/components/packs/packs.module.ts
+++ src/app/components/packs/packs.module.ts
@@ -6,13 +6,14 @@ import { PacksComponent } from './packs.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AuthRESTService } from '@app/services/authREST.service';
-
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     PacksComponent
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     PacksRoutingModule,
     FormsModule,
     HttpClientModule
